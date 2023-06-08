e41249c36822e9ac4a58077c14debb11805d8f9e
pabloboer <boer.pablo@gmail.com>
2023-06-08 00:59:02 -0300

    commit inicial proyecto

---
+++ api/src/main/java/foroAlura/api/controller/TopicosController.java
@@ -0,0 +1,17 @@
+package foroAlura.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/topicos")
+public class TopicosController {
+
+    @GetMapping
+    public String hola(){
+        return "Hola";
+    }
+
+
+}
